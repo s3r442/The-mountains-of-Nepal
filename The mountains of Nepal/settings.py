a329f226411d6088beec1a6b2df64be2ed76f602
Sergey <s3rzhik23@yandex.ru>
2025-11-30 01:57:48 +0300

    maked homepage app

--- The mountains of Nepal/settings.py
+++ The mountains of Nepal/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'm_app.apps.MAppConfig',
+    'homepage.apps.HomepageConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- homepage/apps.py
+++ homepage/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class MAppConfig(AppConfig):
+class HomepageConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'm_app'
+    name = 'homepage'
