8f49a11fcbf1bf9581c8a3f450e46907ebe44b14
Sergey <s3rzhik23@yandex.ru>
2025-11-30 10:59:02 +0300

    full nepal mount page

--- nepal_mounts/views.py
+++ nepal_mounts/views.py
@@ -8,4 +8,23 @@ def index(request):
 def mount_page(request,pk):
     if pk == 'chomolungma':
         return render(request, 'chomolungma.html')
+    elif pk == 'kanchenjunga':
+        return render(request, 'kanchenjunga.html')
+    elif pk == 'lhotse':
+        return render(request, 'lhotse.html')
+    elif pk == 'makalu':
+        return render(request, 'makalu.html')
+    elif pk == 'cho_oyo':
+        return render(request, 'cho_oyo.html')
+    elif pk == 'dhaulagiri':
+        return render(request, 'dhaulagiri.html')
+    elif pk == 'manaslu':
+        return render(request, 'manaslu.html')
+    elif pk == 'annapurna':
+        return render(request, 'annapurna.html')
+    elif pk == 'ama_dablam':
+        return render(request, 'ama_dablam.html')
+
+
+
 
